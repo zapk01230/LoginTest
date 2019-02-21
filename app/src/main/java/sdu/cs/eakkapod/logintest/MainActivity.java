05dd9ef490050298be18076f11139173a4d6030c
Eakkapod <zaeak.123@gmail.com>
2019-02-21 10:07:52 +0700

    Check ค่าว่าง

--- app/src/main/java/sdu/cs/eakkapod/logintest/MainActivity.java
+++ app/src/main/java/sdu/cs/eakkapod/logintest/MainActivity.java
@@ -2,12 +2,44 @@ package sdu.cs.eakkapod.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    String nameString,userString, passString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ผูกตัวแปลบน Java กับ xml
+        final EditText nameEditText     = findViewById(R.id.edtName);
+        final EditText userEditText     = findViewById(R.id.edtUsername);
+        final EditText passEditText     = findViewById(R.id.edtPassword);
+        Button loginButton = findViewById(R.id.btnLoing);
+
+        //onClick Login Button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passString = passEditText.getText().toString().trim();
+
+                //Check ค่าว่าง
+                if ((nameString.length()==0)||(userString.length()==0)||(passString.length()==0)) {
+                    Toast.makeText(getApplicationContext(), "กรอกข้อมูลให้ครบทุกช่อง", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });//end OnClickListener
+
     }//end onCreate
+
 }//end class
