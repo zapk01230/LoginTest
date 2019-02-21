f947af077d46693cd5f37cf3162b42c3b65c7189
Eakkapod <zaeak.123@gmail.com>
2019-02-21 10:24:38 +0700

    Check Username and Password

--- app/src/main/java/sdu/cs/eakkapod/logintest/MainActivity.java
+++ app/src/main/java/sdu/cs/eakkapod/logintest/MainActivity.java
@@ -37,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "กรอกข้อมูลให้ครบทุกช่อง", Toast.LENGTH_SHORT).show();
                 }
 
+                //Check Username and Password
+                if ((userString.equals("admin")) && (passString.equals("1234"))) {
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                } else {Toast.makeText(getApplicationContext(),"กรอกข้อมูลไม่ถูกต้อง",Toast.LENGTH_SHORT).show();
+
+                }
+
             }
         });//end OnClickListener
 
